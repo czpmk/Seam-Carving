c091d6f4154dd2b0891d5d7d09d8ca9490df88fe
czpmk <czapiewskimk@gmail.com>
2020-10-12 19:35:54 +0200

    feat(Element.kt): new field: pixel, energySum = maxDouble by default

--- src/Element.kt
+++ src/Element.kt
@@ -1,5 +1,5 @@
-class Element(energy: Double, val x: Int, val y: Int) {
+class Element(val pixel: Pixel, val x: Int, val y: Int) {
     var checked = false
-    var energySum: Double = energy
+    var energySum: Double = Double.MAX_VALUE
     var parent: Element? = null
 }
