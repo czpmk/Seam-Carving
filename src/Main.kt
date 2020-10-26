667d92977ff62668fdd61a9650b5be8f918c6e5e
czpmk <czapiewskimk@gmail.com>
2020-10-26 14:43:21 +0100

    feat(Main.kt): new functionality: time measurement

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.time.measureTime
 
-const val inputPath = "/home/mc/IntelliJProjects/Seam-Carving/source_images/trees.png"
+const val inputPath = "/home/mc/IntelliJProjects/Seam-Carving/source_images/blue.png"
 const val outputPath = "output_images/trees-out.png"
 var inputFile = File(inputPath)
 var outputFile = File(outputPath)
@@ -18,10 +19,11 @@ fun saveImage(image: BufferedImage) {
 fun pipeline() {
     val inputImage: BufferedImage = loadImage()
     val seamFinder = SeamFinder(inputImage)
-    val outputImage = seamFinder.reduceSize(1, 0)
+    val outputImage = seamFinder.removeLines(150, 50)
     saveImage(outputImage)
 }
 
 fun main() {
-    pipeline()
+    val time = measureTime { pipeline() }
+    println(time)
 }
