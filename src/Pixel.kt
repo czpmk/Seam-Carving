bd2e1a066884dfd948bfe36768b186435a1c497d
czpmk <czapiewskimk@gmail.com>
2020-10-27 18:51:41 +0100

    feat(Pixel.kt): new fields: energySum, predecessor

--- src/Pixel.kt
+++ src/Pixel.kt
@@ -2,4 +2,6 @@ import java.awt.Color
 
 class Pixel(intColor: Int) : Color(intColor) {
     var energy: Double = Double.MAX_VALUE
+    var energySum: Double = Double.MAX_VALUE
+    var predecessor: Coordinates? = null
 }
